4fc601c22d7795ecc0486e0569f6968cbf6d6e8a
adam <19adamchristie96@gmail.com>
2024-08-04 12:09:55 +0100

    Add node class to be used for each key value pair of our HashMap

---
+++ Node.js
@@ -0,0 +1,31 @@
+class Node{
+    constructor(key = null, value = null, next = null){
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    /**
+     * Returns the key of the node
+     * @returns - The key of the Node
+     */
+    getKey(){
+        return this.key;
+    }
+
+    /**
+     * Returns the value of the node
+     * @returns - The value of the Node
+     */
+    getValue(){
+        return this.value;
+    }
+
+    /**
+     * Sets the next property of this node to the given node
+     * @param {*} node - The node that we want this node to point to
+     */
+    setNext(node){
+        this.next = node;
+    }
+}
